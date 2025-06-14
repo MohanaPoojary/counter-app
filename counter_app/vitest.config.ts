70efe0cc4e449abcd0e02a0a089616a100421b54
MohanaPoojary <mohan9980548223@gmail.com>
2025-06-14 15:09:33 +0530

    update

--- counter_app/vitest.config.ts
+++ counter_app/vitest.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/counter-app/",
+  base: '/counter-app/',
   test: {
     globals: true,
     environment: 'jsdom',
