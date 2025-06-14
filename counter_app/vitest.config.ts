c587fc3085829a19c4dbc743aa4b8aa20340d40a
MohanaPoojary <mohan9980548223@gmail.com>
2025-06-14 15:17:31 +0530

    update

--- counter_app/vitest.config.ts
+++ counter_app/vitest.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  base: '/counter-app/',  // <-- make sure this ends with a slash
   plugins: [react()],
-  base: '/counter-app/',
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './test/setup.ts',
     css: true,
-        coverage: {
-            reporter: ["text", "lcov", "html"], // include 'html' reporter
-            exclude: ['src/main.tsx']
+    coverage: {
+      reporter: ['text', 'lcov', 'html'],
+      exclude: ['src/main.tsx'],
+    },
   },
-}
-});
+})
