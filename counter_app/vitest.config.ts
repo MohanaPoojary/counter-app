950e5f5b990c6ddf30e9d39aca13b602dc97bcf8
MohanaPoojary <mohan9980548223@gmail.com>
2025-06-14 15:18:56 +0530

    config update

--- counter_app/vitest.config.ts
+++ counter_app/vitest.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  base: '/counter-app/',  // <-- make sure this ends with a slash
+  base: '/counter-app/',  
   plugins: [react()],
   test: {
     globals: true,
